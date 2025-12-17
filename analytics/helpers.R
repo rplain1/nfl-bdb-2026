e6ff777f86ad978ea94c67fba3e575fc2bc0fc2b
rplain1 <ryan.plain1@gmail.com>
2025-12-17 15:25:48 -0700

    last touches

--- analytics/helpers.R
+++ analytics/helpers.R
@@ -9,16 +9,18 @@ play_filter <- function(.data, play) {
 
 get_common_plays <- function() {
   tribble(
-    ~game_id   , ~play_id , ~player_name      ,
-    2023092408 ,     1574 , "Trent Sherfield" ,
-    2023091706 ,     2583 , "Mike Evans"      ,
-    2023091710 ,     3659 , "Garrett Wilson"  ,
-    2023101506 ,     3367 , "Josh Downs"      ,
-    2023092406 ,     2741 , "Mike Williams"   ,
-    2023091706 ,     3512 , "Chase Claypool"  ,
-    2023090700 ,      361 , "Marvin Jones"    ,
-    2023100808 ,     3368 , "A.J. Brown"      ,
-    2023090700 ,      101 , "Josh Reynolds"
+    ~game_id   , ~play_id , ~player_name        ,
+    2023092408 ,     1574 , "Trent Sherfield"   ,
+    2023091706 ,     2583 , "Mike Evans"        ,
+    2023091710 ,     3659 , "Garrett Wilson"    ,
+    2023101506 ,     3367 , "Josh Downs"        ,
+    2023092406 ,     2741 , "Mike Williams"     ,
+    2023091706 ,     3512 , "Chase Claypool"    ,
+    2023090700 ,      361 , "Marvin Jones"      ,
+    #2023100808 ,     3368 , "A.J. Brown"      ,
+    2023090700 ,      101 , "Josh Reynolds"     ,
+    2023121011 ,     2721 , "Amon-Ra St. Brown" ,
+    2024010713 ,      746 , "Stefon Diggs"
   )
 }
 
--- analytics/main.R
+++ analytics/main.R
@@ -24,7 +24,11 @@ tbl(con, "coverage_predictions") |>
   ungroup() |>
   group_by(game_id, play_id, nfl_id, nfl_id_def) |>
   ungroup() |>
-  compute(name = "coverage_predictions_stg_2", overwrite = TRUE)
+  compute(
+    name = "coverage_predictions_stg_2",
+    overwrite = TRUE,
+    temporary = FALSE
+  )
 
 
 df <- tbl(con, "coverage_predictions_stg_2") |>
@@ -47,12 +51,19 @@ df <- tbl(con, "coverage_predictions_stg_2") |>
     accel_diff = accel_change - accel_change_def,
     cov_max = max(pred_coverage)
   ) |>
-  ungroup()
+  ungroup() |>
+  filter(cov_max > 0.2)
 
 df |>
   #inner_join(df |> distinct(game_id, play_id) |> slice_sample(n = 1)) |>
   play_filter(get_common_plays()) |>
   collect() |>
+  group_by(game_id, play_id, nfl_id, nfl_id_def) |>
+  arrange(frame_id) |>
+  mutate(
+    z_turn_ra = (z_turn_play + lag(z_turn_play) + +lag(z_turn_play, 2)) / 3
+  ) |>
+  ungroup() |>
   select(
     game_id,
     frame_id,
@@ -73,20 +84,19 @@ df |>
     z_turn_play
   ) |>
   pivot_longer(cov_max:z_turn_play) |>
-  filter(str_detect(name, "pred") | name == 'cov_max') |>
-  #filter( name %in% c('a', 'a_def')) |>
+  #filter(str_detect(name, "pred") | name == 'cov_max') |>
+  filter(str_detect(name, "pred")) |>
   #filter(frame_id > 10) |>
   ggplot(aes(frame_id, value, color = name)) +
   geom_line() +
   #geom_hline(yintercept = c(-2, 2), linetype = 'dashed') +
   facet_wrap(
     ~ paste(player_name, player_name_def, sep = '\n'),
-    scales = 'free'
+    scales = 'free_x'
   )
-
+scale_color_manual(values = c('grey', 'grey', 'red'))
 
 dt <- 0.1 # 10 Hz tracking
-contest_dist <- 1.0
 
 df |>
   group_by(
@@ -103,10 +113,10 @@ df |>
   mutate(
     sep_trigger = abs(z_turn_pop) > 2.5 &
       abs(z_turn_play) > 2 &
-      lag(z_turn_play) < 2,
-    sep_id = cumsum(sep_trigger)
+      lag(abs(z_turn_play)) < 2,
+    sep_id = cumsum(sep_trigger),
+    distance_diff = distance - lag(distance)
   ) |>
-  filter(sep_id > 0) |>
   collect() |>
   group_by(
     game_id,
@@ -119,22 +129,80 @@ df |>
     player_position_def,
     sep_id
   ) |>
+  arrange(.by_group = T) |>
   mutate(
     reverse_cummax = rev(cummax(rev(distance))),
     delta_recovery = reverse_cummax - distance,
     impact_contrib = delta_recovery * dt,
-  ) |>
+    min_speed_post_break = min(
+      s_def[frame_id <= frame_id[which.max(distance)]],
+      na.rm = TRUE
+    ),
+    speed_at_break = first(s_def)
+  ) -> df_stg
+
+
+df_stg |>
   summarise(
+    break_frame = first(frame_id),
     max_distance = max(distance),
+    min_distance = min(distance),
     recovery_auc = sum(impact_contrib, na.rm = TRUE),
     peak_recovery_gap = max(delta_recovery, na.rm = TRUE),
     recovery_frames = sum(delta_recovery > 0, na.rm = TRUE),
+    speed_lost = max(speed_at_break, na.rm = TRUE),
     .groups = "drop"
   ) |>
+  filter(!(recovery_frames == 0 & sep_id > 0)) |>
   mutate(
     ESS = max_distance + peak_recovery_gap,
     ISI = max_distance + recovery_auc,
-    SEP_DELTA = ISI - max_distance
+    SEP_DELTA = ISI - max_distance,
+    across(c(ESS, ISI, SEP_DELTA), ~ .x * (sep_id > 0)),
+    across(c(ESS, ISI, SEP_DELTA), ~ replace_na(.x, 0))
   ) -> .df
 
-.df
+.df |> filter(sep_id > 0)
+
+
+.df |>
+  filter(sep_id > 0) |>
+  inner_join(get_common_plays())
+
+
+.df |>
+  summarise(
+    sep = max_distance + ISI,
+    .by = c(game_id, play_id, nfl_id, player_name, player_position)
+  ) |> #count(win) |> mutate(perc = win / n)
+  summarise(
+    n = n(),
+    wins = mean(sep),
+    #across(c(ESS, ISI, SEP_DELTA), list(mean = mean)),
+    .by = c(nfl_id, player_name, player_position)
+  ) |>
+  arrange(-wins) |>
+  filter(n > 100)
+
+
+.df |>
+  filter(player_position %in% c('WR')) |>
+  summarise(
+    win = SEP_DELTA > 0,
+    .by = c(game_id, play_id, nfl_id, player_name, player_position)
+  ) |> #count(win) |> mutate(perc = win / n)
+  summarise(
+    n = n(),
+    wins = sum(win),
+    #across(c(ESS, ISI, SEP_DELTA), list(mean = mean)),
+    .by = c(nfl_id, player_name, player_position)
+  ) |>
+  arrange(-wins) |>
+  mutate(
+    perc = wins / n
+  ) |>
+  arrange(-perc) |>
+  filter(n > 100)
+ggplot(aes(n, wins)) +
+  geom_point() +
+  ggrepel::geom_text_repel(aes(label = player_name))
